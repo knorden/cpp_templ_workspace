96aa7f37405ae8e8847fdbc2a083cfc942428950
Karl Norden <knorden@github.com>
2020-10-04 09:06:29 -0700

    Setting up Git repo

---
+++ README.md
@@ -0,0 +1,4 @@
+#
+
+---
+This is workspace folder for assignment:
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string tString = "THIS IS A TEST STRING.";
+	std::cout << tString << std::endl;
+
+	return 0;
+}
