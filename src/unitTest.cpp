60245af8f185332b20fc3f7b2e6c8c1fa0bbf636
cnorden <git@gitlab.eecs.wsu.edu>
2020-10-15 08:35:55 -0700

    tweak

--- src/unitTest.cpp
+++ src/unitTest.cpp
@@ -1,12 +1,15 @@
 #include "unitTest.hpp"
 
 bool unitTestFunc() {
-  /* DECLARE A PASSMARK FLAG */
+  /* Declare parameters for Passmark Flag & Test IDs */
   static bool allPassed = true;
   static std::string thisTest = "";
+  static int testNum = 0;
 
   /* INIT TEST: OUTPUT STRING */
   thisTest = "INIT OUTPUT";
+  std::cout << "\n\nTEST #" << testNum << ": " + thisTest << std::endl;
+  std::cout << "---------------------------------------------\n";
   allPassed = []() {
     std::string tString = "THIS IS A TEST STRING.";
     std::cout << tString << std::endl;
@@ -15,8 +18,16 @@ bool unitTestFunc() {
   errCatch(allPassed, thisTest);
 
   /* TEST: */
+  thisTest = "";
+  ++testNum;
+  std::cout << "\n\nTEST #" << testNum << ": " + thisTest << std::endl;
+  std::cout << "---------------------------------------------\n";
 
   /* TEST: */
+  thisTest = "";
+  ++testNum;
+  std::cout << "\n\nTEST #" << testNum << ": " + thisTest << std::endl;
+  std::cout << "---------------------------------------------\n";
 
   return allPassed;
 }
